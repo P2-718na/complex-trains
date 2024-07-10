7ebaaa7bcf6045b372cc55a9a59575020a734349
Matteo Bonacini <26163503+P2-718na@users.noreply.github.com>
2024-07-10 14:22:01 +0200

    Add city filtering

---
+++ scripts/trainline-2-filter-out-cities.js
@@ -0,0 +1,33 @@
+const fetch = require("node-fetch-commonjs")
+const { createWriteStream } = require("fs")
+
+const { batchedPromiseAll } = require('batched-promise-all')
+const { ensureDirSync, readdirSync, readFileSync, writeJsonSync, appendFileSync, removeSync } = require("fs-extra")
+const {remove} = require("fs-extra/lib/remove");
+
+removeSync("../data/trainline/cities")
+removeSync("../data/trainline/errors.txt")
+ensureDirSync("../data/trainline/cities")
+
+let stations = readdirSync("../data/trainline/stations")
+
+stations.forEach(async fileName => {
+    const html = await readFileSync(`../data/trainline/stations/${fileName}`)
+    const re = /^.*var __STATE__ = (\{.*\}).*$/m
+    const result = re.exec(html)
+    let state;
+
+    try {
+        state = JSON.parse(result[1])
+    } catch (_) {
+        appendFileSync("../data/trainline/errors.txt", `${fileName}\n`)
+        console.error("Error:", fileName)
+        return;
+    }
+
+    if (!state.origin?.isCity) {
+        return;
+    }
+
+    writeJsonSync(`../data/trainline/cities/${fileName}.json`, state)
+})
